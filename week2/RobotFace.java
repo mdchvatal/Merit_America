786eb12de3f71befdf0164a88aca7830de4d70c1
Matt Chvatal <mdchvatal@gmail.com>
2021-02-18 18:58:26 -0500

    added interactive feature

--- week2/RobotFace.java
+++ week2/RobotFace.java
@@ -1,24 +1,38 @@
 package week2;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class RobotFace extends GraphicsProgram {
     public static final int HEAD_HEIGHT = 400;
     public static final int HEAD_WIDTH = 300;
-    public static final double EYE_RADIUS = HEAD_WIDTH / 7;
+    public static final int EYE_RADIUS = HEAD_WIDTH / 7;
     public static final int MOUTH_WIDTH = (HEAD_WIDTH / 4) *3;
     public static final int MOUTH_HEIGHT = HEAD_HEIGHT / 6;
 
-    public void run() {
-        createHead();
-        createEyes();
-        createMouth();
+    int click_counter = 0;
+
+    public void init() {
+        addMouseListeners();
     }
 
+    public void mouseClicked(MouseEvent e) {
+        if (click_counter == 0) {
+            createHead();
+            click_counter++;
+        } else if (click_counter == 1) {
+            createEyes();
+            click_counter++;
+        } else {
+            createMouth();
+            click_counter++;
+        }
+    }
 
     private void createHead() {
         GRect head = new GRect(((getWidth()-HEAD_WIDTH)/2), ((getHeight()-HEAD_HEIGHT)/2) , HEAD_WIDTH, HEAD_HEIGHT);
