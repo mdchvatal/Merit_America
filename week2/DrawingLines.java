b7b1cddae34d96831eed64d082a08c022bbc17e6
Matt Chvatal <mdchvatal@gmail.com>
2021-02-18 14:12:11 -0500

    completed DrawingLines.java

--- week2/DrawingLines.java
+++ week2/DrawingLines.java
@@ -1,7 +1,23 @@
 package week2;
 
+import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
+import java.awt.event.*;
 
 public class DrawingLines extends GraphicsProgram {
+    private GLine line;
+
+    public void init() {
+        addMouseListeners();
+    }
+
+    public void mousePressed(MouseEvent e) {
+        line = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+        add(line);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        line.setEndPoint(e.getX(), e.getY());
+    }
 
 }
