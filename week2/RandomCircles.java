62e69e22f21765ce2c711fb8a416958265acab75
Matt Chvatal <mdchvatal@gmail.com>
2021-02-18 12:39:17 -0500

    completed RandomCircles.java

--- week2/RandomCircles.java
+++ week2/RandomCircles.java
@@ -1,7 +1,29 @@
 package week2;
 
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+import java.awt.*;
 
 public class RandomCircles extends GraphicsProgram {
+    RandomGenerator rgen = new RandomGenerator();
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            genCircle();
+        }
+    }
+
+    private void genCircle() {
+        int diameter = rgen.nextInt(10, 100);
+        double xAxis = rgen.nextDouble(0, (getWidth()-diameter));
+        double yAxis = rgen.nextDouble(0, (getHeight()-diameter));
+        Color color = rgen.nextColor();
+        GOval circle = new GOval(xAxis, yAxis, diameter, diameter);
+        circle.setFilled(true);
+        circle.setFillColor(color);
+        add(circle);
+    }
 
 }
