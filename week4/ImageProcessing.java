23b078995cf3187045cf7eadcfe1ea18240ad607
Matt Chvatal <mdchvatal@gmail.com>
2021-03-02 11:01:06 -0500

    Completed

--- week4/ImageProcessing.java
+++ week4/ImageProcessing.java
@@ -1,7 +1,25 @@
 package week4;
 
+import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
 
-public class ImageProcessing extends GraphicsProgram {
 
+public class ImageProcessing extends GraphicsProgram {
+	public void run() {
+		GImage image = new GImage("Vermeer_-_The_Milkmaid.jpg");
+		GImage reverseImage = new GImage(flipHorizontal(image));
+		add(image);
+		add(reverseImage, (image.getWidth()), 0);
+	}
+
+	int[][] flipHorizontal(GImage image) {
+		int[][] tempArray = image.getPixelArray();
+		int[][] intArray = image.getPixelArray();
+		for (int i = 0; i < intArray.length; i++) {
+			for (int j = 0; j < intArray[i].length; j++) {
+				tempArray[i][j] = intArray[i][intArray[i].length - (j+1)];
+			}
+		}
+		return tempArray;
+	}
 }
--- week4/NameCounts.java
+++ week4/NameCounts.java
@@ -2,10 +2,42 @@ package week4;
 
 import acm.program.ConsoleProgram;
 
+import java.util.HashMap;
+
 public class NameCounts extends ConsoleProgram {
+
+	private HashMap<String, Integer> nameList = new HashMap<>();
 	
 	public void run() {
-
+		readNames();
+		printNames(nameList);
 	}
-	
+
+	public void readNames() {
+		while (true) {
+			String nextName = readLine("Enter a name. " + "\n" +
+					"If there are no new names, press \"Enter\": ");
+			if (nextName.equals("")) break;
+			nameOrIncrement(nextName);
+		}
+	}
+
+	public void nameOrIncrement(String name) {
+		if (!nameList.containsKey(name)) {
+			nameList.put(name, 1);
+		} else {incrementNameCount(name);}
+	}
+
+	public void incrementNameCount(String name) {
+		int tempCount = nameList.get(name);
+		tempCount++;
+		nameList.put(name, tempCount);
+	}
+
+	public void printNames(HashMap<String, Integer> list) {
+		list.forEach((String, Integer) -> {
+			println("Entry " + String + " has count " + Integer);
+		});
+	}
+
 }
